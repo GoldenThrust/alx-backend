b2c5676634b35c906dddc61bc0cfc02461229007
Adeniji Olajide <adenijiolajid01@gmail.com>
2024-02-01 02:16:13 +0100

    No Commit Message

--- 0x01-caching/3-lru_cache.py
+++ 0x01-caching/3-lru_cache.py
@@ -14,16 +14,14 @@ class LRUCache(BaseCaching):
 
     def put(self, key, item):
         """Add an item in the cache"""
-        if key is None or item is None:
-            return
-
-        self.cache_data[key] = item
-        self.access_key[key] = 0
-        if len(self.cache_data) > self.MAX_ITEMS:
-            min_key = min(self.access_key, key=self.access_key.get)
-            self.cache_data.pop(min_key)
-            self.access_key.pop(min_key)
-            print("DISCARD: {}".format(min_key))
+        if key or item:
+            self.cache_data[key] = item
+            self.access_key[key] = 0
+            if len(self.cache_data) > self.MAX_ITEMS:
+                min_key = min(self.access_key, key=self.access_key.get)
+                self.cache_data.pop(min_key)
+                self.access_key.pop(min_key)
+                print("DISCARD: {}".format(min_key))
 
     def get(self, key):
         """Get an item by key"""
