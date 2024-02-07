d162e7a9b1285e0aec35678725d57257340a730b
Adeniji Olajide <adenijiolajid01@gmail.com>
2024-02-07 01:14:33 +0100

    Locale

---
+++ 0x02-i18n/4-app.py
@@ -0,0 +1,32 @@
+from flask import Flask, render_template, request, g
+from flask_babel import Babel
+
+
+app = Flask(__name__)
+babel = Babel(app)
+
+class Config:
+    """ app configuration """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+app.config.from_object(Config)
+
+@babel.localeselector
+def get_locale():
+    """ Retrieves the best match with
+    our supported languages. """
+    user = getattr(g, 'user', None)
+    if user is None:
+        if user.local in app.config["LANGUAGES"]:
+            return user.locale
+    return request.accept_languages.best_match(app.config['LANGUAGES'])
+
+@app.route('/')
+def index():
+    """The home page."""
+    return render_template('3-index.html')
+
+if __name__ == '__main__':
+    app.run(debug=True)
