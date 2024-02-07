57f3ca8be98ca4bd254062ea4e8000c3324966b5
Adeniji Olajide <adenijiolajid01@gmail.com>
2024-02-07 01:15:50 +0100

    Template

--- 0x02-i18n/4-app.py
+++ 0x02-i18n/4-app.py
@@ -26,7 +26,7 @@ def get_locale():
 @app.route('/')
 def index():
     """The home page."""
-    return render_template('3-index.html')
+    return render_template('4-index.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
