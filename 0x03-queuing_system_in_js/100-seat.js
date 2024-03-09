c1e7dae63737eda0cf7405c87c1156de6eba4ac9
Adeniji Olajide <adenijiolajid01@gmail.com>
2024-03-09 19:53:56 +0100

    process

--- 0x03-queuing_system_in_js/100-seat.js
+++ 0x03-queuing_system_in_js/100-seat.js
@@ -1,11 +1,98 @@
+import express from 'express';
+import { promisify } from 'util';
+import { createQueue } from 'kue';
 import { createClient } from 'redis';
 
-const client = createClient();
+const app = express();
+const redisClient = createClient({ name: 'reserve_seat' });
+const queue = createQueue();
+const initialSeats = 50;
+let reservationEnabled = true;
+const PORT = 1245;
 
-client.on('error', (err) => {
-  console.log('Redis client not connected to the server:', err.toString());
+async function reserveSeat(number) {
+  const setAsync = promisify(redisClient.SET).bind(redisClient);
+
+  try {
+    const response = await setAsync('available_seats', number);
+    return response;
+  } catch (err) {
+    return err;
+  }
+};
+
+
+async function getCurrentAvailableSeats() {
+  const getAsync = promisify(redisClient.GET).bind(redisClient);
+
+  try {
+    const response = await getAsync('available_seats');
+    return response;
+  } catch (err) {
+    return err;
+  }
+};
+
+app.get('/available_seats', (req, res) => {
+  getCurrentAvailableSeats()
+    .then((numberOfAvailableSeats) => {
+      res.json({ numberOfAvailableSeats })
+    });
 });
 
-client.on('connect', () => {
-  console.log('Redis client connected to the server');
+app.get('/reserve_seat', (_req, res) => {
+  if (reservationEnabled) {
+    try {
+      const job = queue.create('reserve_seat');
+
+      job.on('complete', () => {
+        console.log(
+          'Seat reservation job',
+          job.id,
+          'completed'
+        );
+      });
+
+      job.on('failed', (err) => {
+        console.log(
+          'Seat reservation job',
+          job.id,
+          'failed:',
+          err.message || err.toString(),
+        );
+      });
+
+      job.save();
+      res.json({ status: 'Reservation in process' });
+    } catch {
+      res.json({ status: 'Reservation failed' });
+    }
+  } else {
+    res.json({ status: 'Reservation are blocked' });
+  }
 });
+
+app.get('/process', (_req, res) => {
+  res.json({ status: 'Queue processing' });
+
+  queue.process('reserve_seat', (job, done) => {
+    getCurrentAvailableSeats()
+      .then((availableSeats) => {
+        availableSeats = Number(availableSeats || 0)
+        reservationEnabled = availableSeats <= 1 ? false : true;
+        if (availableSeats >= 1) {
+          reserveSeat(availableSeats - 1)
+            .then(() => done());
+        } else {
+          done(new Error('Not enough seats available'));
+        }
+      });
+  });
+});
+
+
+app.listen(PORT, () => {
+  reserveSeat(initialSeats)
+});
+
+export default app;
--- 0x03-queuing_system_in_js/9-stock.js
+++ 0x03-queuing_system_in_js/9-stock.js
@@ -1,11 +1,111 @@
+import express from 'express';
+import { promisify } from 'util';
 import { createClient } from 'redis';
 
-const client = createClient();
+const app = express();
+const redisclient = createClient();
+const PORT = 1245;
 
-client.on('error', (err) => {
-  console.log('Redis client not connected to the server:', err.toString());
-});
+app.use(express.json())
 
-client.on('connect', () => {
-  console.log('Redis client connected to the server');
-});
+const listProducts = [
+  {
+    itemId: 1,
+    itemName: 'Suitcase 250',
+    price: 50,
+    initialAvailableQuantity: 4
+  },
+  {
+    itemId: 2,
+    itemName: 'Suitcase 450',
+    price: 100,
+    initialAvailableQuantity: 10
+  },
+  {
+    itemId: 3,
+    itemName: 'Suitcase 650',
+    price: 350,
+    initialAvailableQuantity: 2
+  },
+  {
+    itemId: 4,
+    itemName: 'Suitcase 1050',
+    price: 550,
+    initialAvailableQuantity: 5
+  },
+];
+
+
+function getItemById(id) {
+  let product = listProducts.find(product => product.itemId === id);
+
+  if (product) return product;
+}
+
+async function reserveStockById(itemId, stock) {
+  const setAsync = promisify(redisclient.SET).bind(redisclient);
+  
+  try {
+    const response = await setAsync(`item.${itemId}`, stock);
+    return response;
+  } catch (err) {
+    return err;
+  }
+}
+
+async function getCurrentReservedStockById(itemId) {
+  const getAsync = promisify(redisclient.GET).bind(redisclient);
+
+  try {
+    const response = await getAsync(`item.${itemId}`);
+    return response;
+  } catch (err) {
+    return err;
+  }
+}
+
+app.get('/list_products', (req, res) => {
+  res.json(listProducts)
+})
+
+app.get('/list_products/:itemId(\\d+)', (req, res) => {
+  const id = req.params.itemId;
+
+  const product = getItemById(Number(id));
+
+  if (product) {
+    getCurrentReservedStockById(id).then(stock => {
+      const reservedStock = Number(stock || 0)
+      product.currentQuantity = product.initialAvailableQuantity - reservedStock;
+      res.json(product)
+    });
+  } else {
+    res.json({ status: 'Product not found' })
+  }
+})
+
+
+app.get('/reserve_product/:itemId(\\d+)', (req, res) => {
+  const id = req.params.itemId;
+
+  const product = getItemById(Number(id));
+
+  if (product) {
+    getCurrentReservedStockById(id).then(stock => {
+      const reservedStock = Number(stock || 0)
+      if (reservedStock < Number(product.initialAvailableQuantity)) {
+        reserveStockById(id, reservedStock + 1).then(() => {
+          res.json({ status: 'Reservation confirmed', id });
+        })
+      } else {
+        res.json({ status: 'Not enough stock available', id });
+        return;
+      }
+    });
+  } else {
+    res.json({ status: 'Product not found' })
+  }
+})
+
+
+app.listen(PORT)
